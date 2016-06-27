1863540238536d4634207608fcdd7a57325582c2
Bennett Joerger <bennettjoerger@gmail.com>
2016-06-27 17:11:31 -0600

    updated routes

--- routes/profile.js
+++ routes/profile.js
@@ -2,13 +2,9 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pitch In' });
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
 });
-
-
-
 
 
 module.exports = router;
