0513c52155e6e41fc778be7aee465335a7dac0ab
Laney Smith <lsmith.unc@gmail.com>
2016-06-27 17:22:55 -0600

    Minor changes

--- routes/profile.js
+++ routes/profile.js
@@ -2,9 +2,25 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
+// Populate profile data on profile page
 router.get('/profile', function(req, res, next) {
-  res.render('profile');
+  knex('account').where({id: req.session.userId}).first().then(function(data) {
+    res.render('profile', {account: data});
+  });
 });
 
+// Populate profile data on profile edit page
+router.get('/profile/edit', function(req, res, next) {
+  knex('account').where({id: req.session.userId}).first().then(function(data) {
+    res.render('edit', {account: data});
+  });
+});
+
+// Edit/update blog post functionality
+router.post('/profile/edit', function(req, res, next) {
+  knex('post').where({id: req.session.userId}).update(req.body).then(function () {
+    res.redirect('/profile');
+  });
+});
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 var db = require('../db/api');
+var auth = require('../auth');
 
 router.get('/volsignup', function(req, res, next) {
   res.render('volsignup');
 });
 router.post('/volsignup', function(req, res, next){
   db.findIdByType(req.body.type).then(function (type){
-    console.log(type)
+    console.log(type);
     return knex('account').insert({email: req.body.email, type: type.id}).returning('id').then(function (id){
       console.log(id);
       return knex('local').insert({account: id[0], password: req.body.password});
