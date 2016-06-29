41390ffec7bd53ec2c68a3eda74d215e4bddc7f6
Laney Smith <lsmith.unc@gmail.com>
2016-06-29 15:00:04 -0600

    Add materialize layout

--- app.js
+++ app.js
@@ -23,12 +23,14 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2]}));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', routes);
 app.use('/', signup);
--- routes/login.js
+++ routes/login.js
@@ -3,23 +3,24 @@ var router = express.Router();
 var knex = require('../db/knex');
 var auth = require('../auth');
 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
 router.post('/login', function (req, res, next) {
   auth.passport.authenticate('local', function(error, user, info){
     if(error){
-      res.render('index', {error: error})
+      res.render('login', {error: error});
     } else if (user){
-      req.session.userId = user.id
-      res.redirect('/profile')
+      req.session.userId = user.id;
+      res.redirect('/profile');
     }
-  })(req, res, next)
-})
+  })(req, res, next);
+});
 
 router.get('/login/logout', function(req, res, next){
-  req.session = null
-  res.redirect('/')
-}) // requires Logout button in profile
-
-
-
+  req.session = null;
+  res.redirect('/');
+}); // requires Logout button in profile
 
 module.exports = router;
