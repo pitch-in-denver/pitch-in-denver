dcfcc326f569bd15763e6ec81c765adb554ff928
Will Thomas <wlthomas001@gmail.com>
2016-06-30 13:46:50 -0600

    add pic

--- routes/events.js
+++ routes/events.js
@@ -6,6 +6,7 @@ var moment = require('moment');
 var auth = require('../auth')
 
 router.get('/events', auth.isNotLoggedIn, function (req, res, next) {
+
   return Promise.all([
     knex('event').select()
     .join('volunteer_activity', function() {
@@ -47,11 +48,14 @@ router.get('/events/:id', auth.isNotLoggedIn, function (req, res, next) {
       .join('account', function() {
         this.on('account.id', '=', 'vol_event.account_id');
       })
+      // .join('volunteer_activity', function() {
+      //   this.on('volunteer_activity.id', '=', 'event.activity_id')
+      // })
       .where({event_id: req.params.id}),
       db.isVolunteer(req.session.userId)
   ]).then(function (data) {
     console.log('data1=', data[1]);
-    res.render('event-detail', {event: data[0], volunteers: data[1], volunteer: data[2], id: req.session.userId});
+    res.render('event-detail', {event: data[0], volunteers: data[1], picture:data[1][0], volunteer: data[2], id: req.session.userId});
   });
 });
 
